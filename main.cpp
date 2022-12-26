f460f2d6c37459703d98e66de64a847a794e94b9
bartdurak <projektator@gmail.com>
2022-12-26 21:36:23 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int myNum = 5;
+    cout <<  myNum;
+    float myFloatNum = 5.99;
+    double myDoubleNum = 9.99;
+    char myCharNum = 'D';
+    bool myBoolNum = true;
+    string myText = "Hello ";
+    return 0;
+}
